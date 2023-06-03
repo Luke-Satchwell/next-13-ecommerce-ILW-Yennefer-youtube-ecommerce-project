cf65a75f684ee382f6b6b24b91c84500d58a5207
Luke Satchwell <satch744@googlemail.com>
2023-06-03 19:29:54 +0100

    next 13 ecommerce ILW Yennefer youtube tutorial

---
+++ app/product/[id]/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="mt-28 flex flex-col items-center justify-center">
+      <h2 className="font-bold text-2xl">Not Found</h2>
+      <p>Could not find the requested resource</p>
+    </div>
+  );
+}
